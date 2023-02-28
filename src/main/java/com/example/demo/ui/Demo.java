c6bbe752c07b500dabd5b04ab585e24e36744534
SatishTejas <tejassatish@hotmail.com>
2023-02-28 18:14:08 +0530

    First Commit

---
+++ src/main/java/com/example/demo/ui/Demo.java
@@ -0,0 +1,22 @@
+package com.example.demo.ui;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class Demo {
+
+	public static Logger logger = LoggerFactory.getLogger(Demo.class);
+
+	@GetMapping
+	public String getName() {
+		logger.info("Demo App");
+		return "Ajay";
+	}
+
+}
+
