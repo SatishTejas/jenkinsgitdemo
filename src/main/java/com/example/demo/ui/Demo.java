28b9ff4539113efbe5023a3719573d6173edab01
Satish <tejassatish@hotmail.com>
2023-03-01 16:17:36 +0530

    Second Commit

--- src/main/java/com/example/demo/ui/Demo.java
+++ src/main/java/com/example/demo/ui/Demo.java
@@ -2,6 +2,7 @@ package com.example.demo.ui;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/demo")
 public class Demo {
+	
+	@Value("${name}")
+	String name;
 
 	public static Logger logger = LoggerFactory.getLogger(Demo.class);
 
 	@GetMapping
 	public String getName() {
 		logger.info("Demo App");
-		return "Ajay";
+		return name;
 	}
 
 }
